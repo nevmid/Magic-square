99e1b740facd65ad5c9c7f7a670306398bd93a52
wawa <147497124+nevmid@users.noreply.github.com>
2024-04-20 09:58:11 +0300

    Added a window with modes

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QWidget, QPushButton
 from PyQt5.Qt import *
 from PyQt5.QtCore import QObject
-import sys
+import sys, random
 
-class Ui_MainWindow(QMainWindow):
+
+class Magic_Square(QMainWindow):
     def __init__(self):
-        super(Ui_MainWindow, self).__init__()
-        self.setupUi()
+        super(Magic_Square, self).__init__()
+        self.size = 0
+        self.range = 0
+        self.x = 0
+        self.y = 0
+        self.setupUi(MainWindow)
 
-    def setupUi(self):
+    def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.setMinimumSize(QtCore.QSize(650, 300))
         self.centralwidget = QWidget(MainWindow)
@@ -54,9 +59,9 @@ class Ui_MainWindow(QMainWindow):
         self.gridLayout_label.addWidget(self.label, 0, 0, 1, 1, QtCore.Qt.AlignHCenter)
         self.gridLayout.addWidget(self.frame_2, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
-        self.inter_menu()
+        self.inter_menu(MainWindow)
 
-    def inter_menu(self):
+    def inter_menu(self, MainWindow):
         MainWindow.setWindowTitle("Магичесий квадрат")
         self.play_button.setText("Играть")
         self.rules_button.setText("Как играть")
@@ -64,14 +69,16 @@ class Ui_MainWindow(QMainWindow):
         self.label.setText("Магический квадрат")
         self.exit_button.clicked.connect(lambda: QApplication.quit())
         self.rules_button.clicked.connect(self.show_rules)
+        self.play_button.clicked.connect(lambda: self.Mode(MainWindow))
+
 
 
     def show_rules(self):
         msg = QMessageBox()
-        msg.setWindowTitle('Правила')
+        msg.setWindowTitle('Как играть')
         msg.setIcon(QMessageBox.Question)
         msg.setWindowIcon(QtGui.QIcon('pr2.png'))
-        msg.setStyleSheet('font: 14pt Arial')
+        msg.setStyleSheet('')
         msg.setText('Магический квадрат - это n * n квадратная сетка, заполненная'
                     ' различными целыми положительными числами в диапазоне 1, 2 , . . . , n *n'
                     ' таким образом, что каждая ячейка содержит целое число, а сумма целых'
@@ -79,11 +86,78 @@ class Ui_MainWindow(QMainWindow):
                     ' чтобы суммы в каждой строке, столбце и диагонали были равными.')
         msg.exec_()
 
+    def Mode(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout_4 = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout_4.setObjectName("gridLayout_4")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.gridLayout = QtWidgets.QGridLayout(self.frame)
+        self.gridLayout.setObjectName("gridLayout")
+        self.pushButton = QtWidgets.QPushButton(self.frame)
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout.addWidget(self.pushButton, 0, 0, 1, 1)
+        self.gridLayout_4.addWidget(self.frame, 0, 0, 1, 1, QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
+        self.frame_2 = QtWidgets.QFrame(self.centralwidget)
+        self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_2.setObjectName("frame_2")
+        self.gridLayout_3 = QtWidgets.QGridLayout(self.frame_2)
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.gridLayout_2 = QtWidgets.QGridLayout()
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.frame_2)
+        self.pushButton_3.setMaximumSize(QtCore.QSize(300, 100))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.gridLayout_2.addWidget(self.pushButton_3, 0, 0, 1, 1)
+        self.pushButton_4 = QtWidgets.QPushButton(self.frame_2)
+        self.pushButton_4.setMaximumSize(QtCore.QSize(300, 100))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.gridLayout_2.addWidget(self.pushButton_4, 1, 0, 1, 1)
+        self.gridLayout_3.addLayout(self.gridLayout_2, 0, 0, 1, 1)
+        self.gridLayout_4.addWidget(self.frame_2, 1, 0, 1, 1)
+        self.frame_3 = QtWidgets.QFrame(self.centralwidget)
+        self.frame_3.setMaximumSize(QtCore.QSize(16777215, 200))
+        self.frame_3.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_3.setObjectName("frame_3")
+        self.gridLayout_4.addWidget(self.frame_3, 2, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.mode_btn(MainWindow)
+
+    def mode_btn(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Магический квадрат"))
+        self.pushButton.setText(_translate("MainWindow", "Меню"))
+        self.pushButton_3.setText(_translate("MainWindow", "Магический"))
+        self.pushButton_4.setText(_translate("MainWindow", "Полумагический"))
+        self.pushButton.clicked.connect(self.__init__)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setStyleSheet('''
+                             QPushButton{
+                             font: 14pt Arial;
+                             background-color: lightgrey;
+                             border-radius: 10px;
+                             border: 2px solid black;
+                             }
+
+                             QPushButton:hover {
+                             background-color:white;
+                             }
+
+                             QPushButton:pressed{
+                             background-color: grey;
+                             }
+                                                     ''')
     MainWindow = QMainWindow()
     MainWindow.resize(1080, 720)
-    ui = Ui_MainWindow()
+    ui = Magic_Square()
     MainWindow.show()
     sys.exit(app.exec_())
